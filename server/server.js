cb8fb16befdec45db91c74f21ddfabcebfaa65bb
Mona Lisa <ravijoshi0790@gmail.com>
2018-12-18 00:13:59 +0530

    Init commit

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const publiPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3329;
 var app = express();
 
 app.use(express.static(publiPath));
 
-app.listen(3000, () => {
+app.listen(3329, () => {
 	console.log(`Server is up on port - ${port}`);
 });
