7d18c3146b20b9dc13520cbb17818b3e12aeacd3
Rohit <rohit.arora52@gmail.com>
2018-12-29 00:39:14 +0530

    Change Port

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3329;
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
